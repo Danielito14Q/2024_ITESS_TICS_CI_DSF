73108454a2502ed840a7f4c92b2fff99cb51e8b7
DanielSalasFlores <danielsalasflores122gmil.com>
2024-02-16 13:42:49 -0600

    Clase SerieTest

---
+++ src/ci/unidad1/aproximation/SerieTest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ci.unidad1.aproximation;
+
+/**
+ *
+ * @author danie
+ */
+public class SerieTest {
+    public static void main(String ... args) {
+        Serie s = new Serie(1,25);
+        
+        s.compute();
+        s.print();
+    }
+}
